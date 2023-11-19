6da01d489f627e15d56fb3ffb10f1bf4277f5a0e
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2023-11-20 03:53:30 +0530

    bug fixed game will works now perfectly

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const computerChoiceDisplay = document.querySelector('#computer-choice')
-const userChoiceDisplay = document.querySelector('#user-choice')
-const result = document.querySelector('#result')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const result = document.getElementById('result')
+console.log(result)
 
 let userInput
 let computerInput
+let gameWinner 
 let buttonValues = []
 const inputOptions = document.querySelectorAll('button')
 
@@ -13,11 +15,29 @@ inputOptions.forEach(inputOption => {
         userInput = e.target.id 
         userChoiceDisplay.textContent = userInput
         computerInput = getComputerGenerateChoice()
+        computerChoiceDisplay.textContent = computerInput
+        checkingWinner()
     })
 })
 
 
 function getComputerGenerateChoice() {
     const randomSelection = buttonValues[Math.floor(Math.random() * buttonValues.length)]
-    computerChoiceDisplay.textContent = randomSelection   
+    return randomSelection
+}
+
+function checkingWinner() {
+    if (userInput == computerInput) {
+        gameWinner = 'game will be draw'
+    } else if (
+        (userInput == 'rock' && computerInput == 'scissors') || 
+        (userInput == 'paper' && computerInput == 'rock') ||
+        (userInput == 'scissors' && computerInput == 'paper')
+    ) {
+        gameWinner = 'You Win..!'
+    } else {
+        gameWinner = 'You Lose..!'
+    }
+
+    result.textContent = gameWinner
 }
